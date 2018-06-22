ddd414865fa5c0bc6da3281588f161db0fa2cf3e
bjohnson <bkj.322@gmail.com>
2018-06-22 12:34:04 -0400

    fixing typo; adding netflix dataset

--- run.sh
+++ run.sh
@@ -10,11 +10,11 @@ mkdir -p {_data,_results}/movielens
 wget --header "Authorization:$TOKEN" https://hiveprogram.com/data/_v1/qmf/movielens/ml-1m-ratings-train.gz
 wget --header "Authorization:$TOKEN" https://hiveprogram.com/data/_v1/qmf/movielens/ml-1m-ratings-test.gz
 
-gunzip ml-*.gz && rm *.gz && mv ml-* _data/movielens
+gunzip ml-*.gz && mv ml-* _data/movielens
 
 ./bin/wals \
-    --train_dataset=tmp-train \
-    --test_dataset=tmp-test \
+    --train_dataset=_data/movielens/ml-1m-ratings-train \
+    --test_dataset=_data/movielens/ml-1m-ratings-test \
     --user_factors=_results/movielens/ml-1m-user \
     --item_factors=_results/movielens/ml-1m-item \
     --nepochs=20 \
@@ -29,3 +29,17 @@ gunzip ml-*.gz && rm *.gz && mv ml-* _data/movielens
 #     --item-path _results/movielens/ml-1m-item
 
 # Run on `ml-100k-ratings` or `ml-20m-ratings` for smaller or larger problem sizes
+
+# ---------------------------------
+# Run on Netflix datasets
+
+mkdir -p {_data,_results}/netflix
+
+wget --header "Authorization:$TOKEN" https://hiveprogram.com/data/_v1/qmf/netflix/netflix.tsv.gz
+gunzip netflix.tsv.gz && mv netflix.tsv _data/netflix
+
+./bin/wals \
+    --train_dataset=_data/netflix/netflix.tsv \
+    --user_factors=_results/movielens/ml-1m-user \
+    --item_factors=_results/movielens/ml-1m-item \
+    --nthreads=32 
